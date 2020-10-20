b524062901e1b1848ac968aa6d0abb5e4c42538b
Jamezjaz <jamezjaz@gmail.com>
2020-10-20 17:47:56 -0500

    docs: adds the setRotation function

--- script.js
+++ script.js
@@ -9,4 +9,12 @@ function setClock() {
   const secondsRatio = currentDate.getSeconds() / 60;
   const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
   const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
 }
+
+function setRotation(ele, rotationRatio) {
+  ele.style.setProperty('--rotation', rotationRatio * 360);
+}
+setClock();
